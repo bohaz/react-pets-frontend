f9eb8ef2d93b788846fed1acccb1557550bed95d
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-10-08 11:10:21 -0300

    Create a new api js file

---
+++ src/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/mascotas';
+
+export const fetchPets = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+export const addPet = async (petData) => {
+  const response = await axios.post(API_URL, petData);
+  return response.data;
+};
+
+export const updatePet = async (id, petData) => {
+  const response = await axios.put(`${API_URL}/${id}`, petData);
+  return response.data;
+};
+
+export const deletePet = async (id) => {
+  await axios.delete(`${API_URL}/${id}`);
+};
